7e587fb2202c1806ca855945ad323819a63f559b
gamingboots <93469894+gamingboots@users.noreply.github.com>
2022-03-22 17:51:16 +0530

    Add files via upload

---
+++ Hello World/helo.cc
@@ -0,0 +1,3 @@
+int main(){
+    std::count << "Hello World!";
+}
---
+++ Hello World/helo.cs
@@ -0,0 +1,7 @@
+namespace HelloWorld{
+    class Hello{
+        static void Main(string[] args) {
+            system.console.WriteLine("Hello World!")
+        }
+    }
+}
