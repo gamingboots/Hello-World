7c3ccb898e0f21fa026b44a98bfd99da7fcf9ab5
athrvvv <codingbruh.ac@gmail.com>
2022-04-12 00:17:02 +0530

    Objective-C lang

--- Hello World/helo.m
+++ Hello World/helo.m
@@ -1,1 +1,8 @@
-disp("Hello World!")
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello World!");
+    }
+    return 0;
+}
